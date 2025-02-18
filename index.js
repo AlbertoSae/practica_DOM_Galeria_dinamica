277f5340a66d091b87bf55ecc078aaee850c5dc8
alber <alberto.saez.canada@gmail.com>
2025-02-18 15:24:36 +0100

    1º

---
+++ index.js
@@ -0,0 +1,99 @@
+const products = [
+    {
+      name: 'HP Essentials 255 G8 AMD',
+      price: 289,
+      stars: 4,
+      reviews: 250,
+      seller: 'PcComponentes',
+      image: 'https://thumb.pccomponentes.com/w-300-300/articles/1005/10057282/1639-hp-essential-255-g8-amd-3020e-8gb-256gb-ssd-156.jpg'
+    },
+    // Añade aquí al menos 9 productos más para tener un total de 10 productos
+    // puedes cambiar los campos de cada objeto si es necesario para tu diseño...
+//1
+    {
+      name: 'Portátil Gaming',
+      price: 999,
+      stars: 4.8,
+      reviews:180,
+      seller: 'PcComponenetes',
+      image:'https://tinyurl.com/c43satjw'
+    },
+    //2
+    {
+      name: 'Monitor 4K',
+      price: 299,
+      stars: 4.8,
+      reviews:180,
+      seller: 'PcComponenetes',
+      image: 'https://www.wepc.com/wp-content/uploads/2020/03/Best-Ultrawide-Gaming-Monitors-2020.jpg '
+    },
+    //3
+    {
+      name: 'Teclado Mecánico',
+      price: 89,
+      stars: 4.4,
+      reviews:380,
+      seller: 'PcComponenetes',
+      image: ' https://www.vsgamers.es/wp-content/uploads/2022/08/Teclado-Mecánico-Gaming-Corsair-K70-PRO-Mini-Wireless-RGB-60.jpg'
+    },
+    //4
+    {
+      name: 'Ratón Gaming',
+      price: 45,
+      stars: 3.9,
+      reviews:580,
+      seller: 'PcComponenetes',
+      image: 'https://www.vsgamers.es/wp-content/uploads/2019/06/Ratón-Gaming-óptico-Logitech-G502-LIGHTSPEED.jpg '
+    },
+    //5
+    {
+      name: 'Tarjeta Gráfica RTX',
+      price: 595,
+      stars: 4,
+      reviews:218,
+      seller: 'PcComponenetes',
+      image: ' https://www.claroshop.com/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/g/e/geforce-rtx-2060-6gb-192-bit-gddr6.jpg'
+    },
+    //6
+    {
+      name: 'Auriculares Inalámbricos',
+      price: 79,
+      stars: 4.6,
+      reviews:1850,
+      seller: 'PcComponenetes',
+      image: ' https://www.falconcomputers.co.uk/wp-content/uploads/2019/07/970-EVO-1TB.jpg'
+    },
+    //7
+    {
+      name: 'Placa Base Gaming',
+      price: 179,
+      stars: 5,
+      reviews:850,
+      seller: 'PcComponenetes',
+      image: 'https://tienda.redcomputer.es/wp-content/uploads/2019/07/MSI-MPG-X570-Gaming-Plus.jpg '
+    },
+    //8
+    {
+      name: 'Procesador Intel i9',
+      price: 499,
+      stars: 4.7,
+      reviews:720,
+      seller: 'PcComponenetes',
+      image: 'https://www.ldlc-pro.com/medias/fiche-technique/BX8070811900F.jpg '
+    },
+    //9
+    {
+      name: 'SSD NVMe 1TB',
+      price: 129,
+      stars: 4.8,
+      reviews:520,
+      seller: 'PcComponenetes',
+      image: ' https://www.falconcomputers.co.uk/wp-content/uploads/2019/07/970-EVO-1TB.jpg'
+    },
+  ];
+
+
+  //hacer un bucle por cada producto y generrar con js en DOm una ficha de producto para ser vista en html
+
+
+  
